5c428f97193ded70a5e5633065b047bae5a67982
Wulfric Wang <lufeihaidao+code@gmail.com>
2016-07-25 16:24:36 +0800

    fix

--- index.js
+++ index.js
@@ -11,17 +11,20 @@
 // ==/UserScript==
 
 (function() {
+  // douban 会自动添加a标签，去掉
   $('#link-report span a').each(function(i, e){
     var c=$(e).text();
     $(e).replaceWith(c);
   });
   var content = $('#link-report span').html();
+  // blockquote
   content = content.replace(/<br>/g,'\n').replace(/&gt;/g,'>');
   var mdf = window.markdownitFootnote;
   var md = window.markdownit().use(mdf);
   var marked_content = md.render(content);
   $('#link-report span').html(marked_content);
 
+  // mathjax
   var head = document.getElementsByTagName("head")[0], script;
   script = document.createElement("script");
   script.type = "text/x-mathjax-config";
